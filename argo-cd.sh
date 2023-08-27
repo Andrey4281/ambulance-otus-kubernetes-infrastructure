2f652f033a06db55788c0b7b8c4c3293433ec534
andrey4281 <Andrey4281@yandex.ru>
2023-08-27 19:16:16 +0200

    Add image updater

--- argo-cd.sh
+++ argo-cd.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 export SOPS_AGE_KEY_FILE=$(pwd)/key.txt
 export SOPS_AGE_RECIPIENTS=age1yu4wcn3ykhlxfkne5te45v9vjcjuuv67h9ywl5kd09fnygr5df8q377uqh
-cd namespaces
-kubectl apply -f argocd-namespace.yaml
+kubectl create namespace argocd
 helm upgrade --install argocd charts/argo-cd -n argocd
-cd ../
 kubectl -n argocd create secret generic helm-secrets-private-keys --from-file=key.txt
 helm secrets -n argocd upgrade --install argocd charts/argo-cd -f values/argocd.yaml
